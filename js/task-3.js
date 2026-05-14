7e6c718cc637bff4f93f8646a73ac2b44621353b
dontcallmenik <dontcallmenik@gmail.com>
2026-05-14 18:56:50 +0300

    7.0
    hw7

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", (event) => {
+  const name = event.currentTarget.value.trim();
+
+  output.textContent = name === "" ? "Anonymous" : name;
+});
---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+});
